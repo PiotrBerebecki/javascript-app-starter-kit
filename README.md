2304f48da69de1b983a1b657cbf4777c79da8ad8
PiotrBerebecki <petebere@gmail.com>
2017-03-18 00:36:57 +0000

    Setup production build

--- README.md
+++ README.md
@@ -8,9 +8,11 @@
 
 Currently handled on Travis CI.
 
+
 ## Code Coverage
 
 Code coverage is setup via npm script and integrated into `.travis.yml` file
+
 
 ## HTTP Calls
 
@@ -20,16 +22,18 @@ Code coverage is setup via npm script and integrated into `.travis.yml` file
 ```
 
 - Serve mock API database (e.g. on http://localhost:3001) with [JSON server](https://github.com/typicode/json-server) (server working on a fake local static JSON database with the ability to add / delete data in the local JSON file).
+App with mock Api is at: http://localhost:3000/?useMockApi=true
+App with real Api is at: http://localhost:3000/
+
 - [JSON Schema Faker](http://json-schema-faker.js.org/)
 - Random data every time the app is started with faker.js, change.js, randexp.js
 
-## Production build
 
+## Production build
 
 - Dist server to debug production build
 - Minification
 - Sourcemaps
 - Dynamic html
-- Cache busting
+- Cache busting by hashing filenames
 - Code splitting
-- Error logging
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,9 +10,10 @@ export default {
   devtool: 'source-map',
   // Display all the files Webpack is bundling.
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   // change target to 'node' or electron if building to other environemntsssssss
   target: 'web',
   // webpack won't generate physical files like bundle.js for
@@ -20,41 +21,41 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
-    // // Generate an external css file with a hash in the filename
-    // new ExtractTextPlugin('[name].[contenthash].css'),
-    //
-    // // Hash the files using MD5 so that their names change when the content changes.
-    // new WebpackMd5Hash(),
-    //
-    // // Use CommonsChunkPlugin to create a separate bundle
-    // // of vendor libraries so that they're cached separately.
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor'
-    // }),
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
 
-    // // Create HTML file that includes reference to bundled JS.
-    // new HtmlWebpackPlugin({
-    //   template: 'src/index.html',
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeRedundantAttributes: true,
-    //     useShortDoctype: true,
-    //     removeEmptyAttributes: true,
-    //     removeStyleLinkTypeAttributes: true,
-    //     keepClosingSlash: true,
-    //     minifyJS: true,
-    //     minifyCSS: true,
-    //     minifyURLs: true
-    //   },
-    //   inject: true,
-    //   // Properties you define here are available in index.html
-    //   // using htmlWebpackPlugin.options.varName
-    //   trackJSToken: 'INSERT YOUR TOKEN HERE'
-    // }),
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: 'INSERT YOUR TOKEN HERE'
+    }),
 
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
@@ -65,7 +66,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 };
